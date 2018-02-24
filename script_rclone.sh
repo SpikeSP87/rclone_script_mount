9566f63feb1bb5c3a6532c56015b6aefe89142df
SpikeSP87 <spikesp87@gmail.com>
2018-02-24 20:11:32 +0100

    Update script_rclone.sh

--- script_rclone.sh
+++ script_rclone.sh
@@ -17,20 +17,20 @@ do
 	then
 		your_remote_name=`echo $line | cut -d $ -f 1`
 		path_in_remote=`echo $line | cut -d $ -f 2`
-       	mount_path=`echo $line | cut -d $ -f 3`
-        mount_options=`echo $line | cut -d $ -f 4`
-        rclone_options=`echo $line | cut -d $ -f 5`
+       		mount_path=`echo $line | cut -d $ -f 3`
+        	mount_options=`echo $line | cut -d $ -f 4`
+        	rclone_options=`echo $line | cut -d $ -f 5`
 		check_mount=$your_remote_name:$path_in_remote
-        is_mount=`mount | grep $check_mount | grep $mount_path`
-        date=`date +%d-%m-%Y:%H:%M:%S`
+        	is_mount=`mount | grep $check_mount | grep $mount_path`
+        	date=`date +%d-%m-%Y:%H:%M:%S`
 
-        if [ "$is_mount" == "" ]
-        then
-               	echo $date": Unidad no montada, realizando montaje del remote: "$your_remote_name:/$path_in_remote
-               	$rclone_path/rclone $rclone_options mount $mount_options $your_remote_name:/$path_in_remote $mount_path &
-        else
-               	echo $date": Montaje del remote "$your_remote_name:/$path_in_remote" correcto."
-        fi
+        	if [ "$is_mount" == "" ]
+        	then
+               		echo $date": Unidad no montada, realizando montaje del remote: "$your_remote_name:/$path_in_remote
+               		$rclone_path/rclone $rclone_options mount $mount_options $your_remote_name:/$path_in_remote $mount_path &
+        	else
+               		echo $date": Montaje del remote "$your_remote_name:/$path_in_remote" correcto."
+        	fi
 
 	fi
 done < $listremotes
